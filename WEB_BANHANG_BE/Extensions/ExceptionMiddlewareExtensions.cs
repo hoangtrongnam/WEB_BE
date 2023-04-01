f8f550a1493842fde8f35dcc11be8ffdf666f4c7
hoangtrongnam <hoangtrongnam9x@gmail.com>
2023-04-01 10:03:26 +0700

    commit code

---
+++ WEB_BANHANG_BE/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace WEB_BANHANG_BE.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, WEB_BANHANG_BE.Services.Logger.ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        // await context.Response.WriteAsync(new WEB_BANHANG_BE.Models.Globally.ErrorModel()
+                        // {
+                        //     Code = "SYS_ERROR",
+                        //     Message = "Internal Server Error."
+                        // }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
--- WEB_BANHANG_BE/Program.cs
+++ WEB_BANHANG_BE/Program.cs
@@ -1,25 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace WebApi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string rootdir = Directory.GetCurrentDirectory();
+            //try
+            //{
+            //    Aspose.Cells.License license = new Aspose.Cells.License();
+            //    string filePath = rootdir + "\\Resources\\" + "Aspose.Total.lic";
+            //    FileStream filestream = new FileStream(filePath, FileMode.Open);
+            //    license.SetLicense(filestream);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("\nThere was an error setting the license: " + ex.Message);
+            //}
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<WEB_BANHANG_BE.Startup>();
+                });
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
